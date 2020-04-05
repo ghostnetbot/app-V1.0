8a5cc31767d50285fc6b1cf3fc2cd8f5568c8885
Ghost <ghostnetautomator@gmail.com>
2020-04-05 13:55:48 -0400

    initial project setup

---
+++ App-V1.0/App-V1.0Tests/App_V1_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  App_V1_0Tests.swift
+//  App-V1.0Tests
+//
+//  Created by Jordan Zeleny on 4/5/20.
+//  Copyright © 2020 Jordan Zeleny. All rights reserved.
+//
+
+import XCTest
+@testable import App_V1_0
+
+class App_V1_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
